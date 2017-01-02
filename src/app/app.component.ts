cd80d023c20cd9ac6b91233a142ea443287d6460
Sean <seon2you@hotmail.com>
2017-01-02 12:30:52 -0800

    marker editable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,11 @@ export class AppComponent {
   lat: number = 33.874958;
   lng: number = -117.966384;
 
+  markerName: string;
+  markerLat: string;
+  markerLng: string;
+  markerDraggable: string;
+
   markers: marker[] = [
     {
       name: 'Company One',
@@ -32,15 +37,60 @@ export class AppComponent {
     }
   ];
 
-  constructor(){
+  constructor() {
 
   }
-  clickedMarker(marker: marker, index: number){
+  clickedMarker(marker: marker, index: number) {
     console.log('Clicked Marker: ' + marker.name + ' at index ' + index);
+  }
+
+  mapClicked($event: any) {
+    console.log('Map Clicked');
+    var newMarker = {
+      name: 'Untitled',
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: false
+    }
+
+    this.markers.push(newMarker);
+  }
+
+  markerDragEnd(marker: any, $event: any){
+    console.log('dragEnd', marker, $event);
+
+    var updMarker = {
+      name: marker.name,
+      lat: parseFloat(marker.lat),
+      lng: parseFloat(marker.lng),
+      draggable: false
+    }
+
+    var newLat = $event.coords.lat;
+    var newLng = $event.coords.lng;
+  }
+
+  addMarker(){
+    console.log('Adding Marker');
+    if(this.markerDraggable == 'yes'){
+      var isDraggable = true;
+    }
+    else{
+      var isDraggable = false;
+    }
+
+    var newMarker = {
+      name: this.markerName,
+      lat: parseFloat(this.markerLat),
+      lng: parseFloat(this.markerLng),
+      draggable: isDraggable
+    }
+
+    this.markers.push(newMarker);
   }
 }
 
-interface marker{
+interface marker {
   name?: string;
   lat: number;
   lng: number;
